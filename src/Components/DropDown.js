a7a1763f19b7157987391adf651f3b0d4b2a1848
123-SHIVA <shivayojha8@gmail.com>
2024-03-08 23:53:54 +0530

    change some content in about page

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,9 +1,9 @@
 
 
 const about = {
-    title: "About ITC",
-    description: "ITC is one of India's foremost private sector companies with a Gross Revenue of ₹ 69,481 crores and Net Profit of ₹ 18,753.31 crores (as on 31.03.2023) with presence in FMCG, Hotels, Packaging, Paperboards & Specialty Papers, Agri & IT Businesses.",
-    button: "Explore About ITC",
+    title: "About ITC ONLINE  ",
+    description: "ITC ONLINE   is one of India's foremost private sector companies with a Gross Revenue of ₹ 69,481 crores and Net Profit of ₹ 18,753.31 crores (as on 31.03.2023) with presence in FMCG, Hotels, Packaging, Paperboards & Specialty Papers, Agri & IT Businesses.",
+    button: "Explore About ITC ONLINE  ",
     ourProfile: ["History and Evolution"],
 
 }
@@ -17,9 +17,9 @@ function DropDown({ componentFor }) {
                 <div className="flex font-semibold gap-10 ">
 
                     <div className="text-[#39A7FF] w-5/12">
-                        <h1 className="text-3xl">About ITC</h1>
-                        <p className="my-10">ITC is one of India's foremost private sector companies with a Gross Revenue of ₹ 69,481 crores and Net Profit of ₹ 18,753.31 crores (as on 31.03.2023) with presence in FMCG, Hotels, Packaging, Paperboards & Specialty Papers, Agri & IT Businesses.</p>
-                        <button className="py-2 px-5 border border-[#39A7FF] rounded-2xl">Explore About ITC</button>
+                        <h1 className="text-3xl">About ITC ONLINE  </h1>
+                        <p className="my-10">ITC ONLINE   is one of India's foremost private sector companies with a Gross Revenue of ₹ 69,481 crores and Net Profit of ₹ 18,753.31 crores (as on 31.03.2023) with presence in FMCG, Hotels, Packaging, Paperboards & Specialty Papers, Agri & IT Businesses.</p>
+                        <button className="py-2 px-5 border border-[#39A7FF] rounded-2xl">Explore About ITC ONLINE  </button>
                     </div>
                     <div className="flex justify-evenly w-7/12">
 
@@ -37,13 +37,13 @@ function DropDown({ componentFor }) {
                                 <li>Code of Conduct</li>
                             </ul>
                             <ul className="font-thin text-xs">
-                                <li className="font-semibold">ITC Research and Development</li>
+                                <li className="font-semibold">ITC ONLINE   Research and Development</li>
                             </ul>
                         </div>
                         <div className="text-gray-700 flex flex-col gap-6 ">
 
                             <ul className="font-thin text-xs">
-                                <li className="font-semibold">ITC Leadership</li>
+                                <li className="font-semibold">ITC ONLINE   Leadership</li>
                                 <li>Board of Directors</li>
                                 <li>CMC</li>
                                 <li>Business Heads</li>
