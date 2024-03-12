7c45c12914495fe9ead27443fd0ca8c6c19bba9b
123-SHIVA <shivayojha8@gmail.com>
2024-03-12 21:32:57 +0530

    changes the number of footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,116 +1,123 @@
 function Footer() {
-    return (
-        <div>
-            <section className="border-t text-sm text-black lg:px-32  px-10">
-                <div className="flex lg:justify-end">
-                    <div className="flex items-center gap-3 p-3">
-                        <img src="https://img.freepik.com/premium-vector/infographic-three-circle-diagram-linked-segments_91606-39.jpg" className="w-10" />
-                        <p>ITC online on social media</p>
-                    </div>
-                    <div className="flex items-center gap-3 p-3 px-5 border-l border-r">
-                        <img src="https://i.pinimg.com/564x/ea/fc/b7/eafcb70d4663faa713ba6d1af348cf9c.jpg"  className="w-10"/>
-                        <p>ITC Online App</p>
-                    </div>
-                    <div className="flex items-center gap-3 p-3">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyhAFmzVyC902J7gP5_Oz2H8_mbqFB0HTAJ7232pLYYqup73OZoXCE1sJ3KEUBNxqrAlw&usqp=CAU" className="w-10" />
-                        <p>Downloads & Websites</p>
-                    </div>
-                </div>
-
-            </section>
-
-            <div className="bg-[#3594e1] text-[#a2ceed] font-semibold hidden lg:block">
-                <div className=" flex text-[11px] w-10/12 m-auto gap-10 px-10 py-14 ">
-                    <ul className="leading-6">
-                        <li className="text-white text-sm font-bold">ITC online LIMITED</li>
-                        <li>Virginia House,</li>
-                        <li>37, J. L. Nehru Road</li>
-                        <li>Kolkata - 700071, India</li>
-                        <li className="text-bold text-white">Ph: +91 9748052028</li>
-                        <li>Working Hours:</li>
-                        <li>(Monday to Friday)</li>
-                        <li>- 9:00AM to 6:00PM</li>
-                        <li>Fax: +91-33-22880655</li>
-                        <li>Corporate Identity Number:</li>
-                        <li>L16005WB1910PLC001985</li>
-                        <li>For any queries or grievances,</li>
-                        <li>contact Mr. Aurko Dasgupta at</li>
-                    </ul>
-
-                    <ul className="leading-6">
-                        <li className="text-white text-sm font-bold">ABOUT ITC ONLINE</li>
-                        <li>Our Profile</li>
-                        <li>Our Values</li>
-                        <li>ITC Online Research and Developement</li>
-                        <li>ITC Online Leadership</li>
-                        <li>Leadership Speaks</li>
-                        <li>Our Policies</li>
-                        <li>Investor Pelations</li>
-                        <li>Awards & Recognitions</li>
-                        <li></li>
-                    </ul>
-                    <ul className="leading-6">
-                        <li className="text-white text-sm font-bold">Business</li>
-                        <li>Fast Moving Consumer</li>
-                        <li>Goods (FMCG)</li>
-                        <li>Hotels</li>
-                        <li>Paperboards &</li>
-                        <li>Speciality Papers</li>
-                        <li>Packaging</li>
-                        <li>Agri Business</li>
-                        <li>Information Technology</li>
-                        <li>Group Companies</li>
-                    </ul>
-                    <ul className="leading-6">
-                        <li className="text-white text-sm font-bold">Sustainability</li>
-                        <li>ITC Online's Sustainability Initiatives</li>
-                        <li>Corporate Social responsibility</li>
-                        <li>Enviroment, Health & Safety</li>
-                        <li>ITC Online's Sustainability Policies</li>
-                        <li>ITC Online CSR Policy</li>
-                        <li>ITC Online Social investments Impact</li>
-                        <li>Studies</li>
-                        <li>Sustainability Report</li>
-                        <li>RFPs For impact Assessnent </li>
-                        <li>& Other Studies</li>
-                    </ul>
-
-                    <ul className="leading-6">
-                        <li className="text-white text-sm font-bold">Media Center</li>
-                        <li>Key Facts About ITC Online</li>
-                        <li>Business Information</li>
-                        <li>Everday Stories From ITC Online</li>
-                        <li>ITC Online in the news</li>
-                        <li>Press Releases</li>
-                        <li>Press Reports</li>
-                        <li>ITC Online Newsroom</li>
-                    </ul>
-                    <ul className="leading-6">
-                        <li className="text-white text-sm font-bold">Brands</li>
-                        <li>Investor relation</li>
-                        <li>Careers</li>
-                        <li>ITC Online Resources</li>
-                        <li>Downloads</li>
-                        <li>Contact Us</li>
-                        <li>Store Locator</li>
-                        <li>ITC Online on Social Media</li>
-                        <li>Sitemap</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div className=" bg-white text-[#6b6b6b] text-xs py-6 border ">
-                <div className="w-10/12 px-10 flex flex-col lg:flex-row justify-between items-center m-auto">
-                    <div >@ ITC Online Limited 2024</div>
-                    <ul className="flex gap-5">
-                        <li>Terms of use</li>
-                        <li>Privacy Policy</li>
-                        <li>Sitemap</li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div>
+      <section className="border-t text-sm text-black lg:px-32  px-10">
+        <div className="flex lg:justify-end">
+          <div className="flex items-center gap-3 p-3">
+            <img
+              src="https://img.freepik.com/premium-vector/infographic-three-circle-diagram-linked-segments_91606-39.jpg"
+              className="w-10"
+            />
+            <p>ITC online on social media</p>
+          </div>
+          <div className="flex items-center gap-3 p-3 px-5 border-l border-r">
+            <img
+              src="https://i.pinimg.com/564x/ea/fc/b7/eafcb70d4663faa713ba6d1af348cf9c.jpg"
+              className="w-10"
+            />
+            <p>ITC Online App</p>
+          </div>
+          <div className="flex items-center gap-3 p-3">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyhAFmzVyC902J7gP5_Oz2H8_mbqFB0HTAJ7232pLYYqup73OZoXCE1sJ3KEUBNxqrAlw&usqp=CAU"
+              className="w-10"
+            />
+            <p>Downloads & Websites</p>
+          </div>
         </div>
-    )
+      </section>
+
+      <div className="bg-[#3594e1] text-[#a2ceed] font-semibold hidden lg:block">
+        <div className=" flex text-[11px] w-10/12 m-auto gap-10 px-10 py-14 ">
+          <ul className="leading-6">
+            <li className="text-white text-sm font-bold">ITC online</li>
+            <li>Virginia House,</li>
+            <li>37, J. L. Nehru Road</li>
+            <li>Kolkata - 700071, India</li>
+            <li className="text-bold text-white">Ph: +91 8981508645</li>
+            <li>Working Hours:</li>
+            <li>(Monday to Friday)</li>
+            <li>- 9:00AM to 6:00PM</li>
+
+            <li>Corporate Identity Number:</li>
+
+            <li>For any queries or grievances,</li>
+          </ul>
+
+          <ul className="leading-6">
+            <li className="text-white text-sm font-bold">ABOUT ITC ONLINE</li>
+            <li>Our Profile</li>
+            <li>Our Values</li>
+            <li>ITC Online Research and Developement</li>
+            <li>ITC Online Leadership</li>
+            <li>Leadership Speaks</li>
+            <li>Our Policies</li>
+            <li>Investor Pelations</li>
+            <li>Awards & Recognitions</li>
+            <li></li>
+          </ul>
+          <ul className="leading-6">
+            <li className="text-white text-sm font-bold">Business</li>
+            <li>Fast Moving Consumer</li>
+            <li>Goods (FMCG)</li>
+            <li>Hotels</li>
+            <li>Paperboards &</li>
+            <li>Speciality Papers</li>
+            <li>Packaging</li>
+            <li>Agri Business</li>
+            <li>Information Technology</li>
+            <li>Group Companies</li>
+          </ul>
+          <ul className="leading-6">
+            <li className="text-white text-sm font-bold">Sustainability</li>
+            <li>ITC Online's Sustainability Initiatives</li>
+            <li>Corporate Social responsibility</li>
+            <li>Enviroment, Health & Safety</li>
+            <li>ITC Online's Sustainability Policies</li>
+            <li>ITC Online CSR Policy</li>
+            <li>ITC Online Social investments Impact</li>
+            <li>Studies</li>
+            <li>Sustainability Report</li>
+            <li>RFPs For impact Assessnent </li>
+            <li>& Other Studies</li>
+          </ul>
+
+          <ul className="leading-6">
+            <li className="text-white text-sm font-bold">Media Center</li>
+            <li>Key Facts About ITC Online</li>
+            <li>Business Information</li>
+            <li>Everday Stories From ITC Online</li>
+            <li>ITC Online in the news</li>
+            <li>Press Releases</li>
+            <li>Press Reports</li>
+            <li>ITC Online Newsroom</li>
+          </ul>
+          <ul className="leading-6">
+            <li className="text-white text-sm font-bold">Brands</li>
+            <li>Investor relation</li>
+            <li>Careers</li>
+            <li>ITC Online Resources</li>
+            <li>Downloads</li>
+            <li>Contact Us</li>
+            <li>Store Locator</li>
+            <li>ITC Online on Social Media</li>
+            <li>Sitemap</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className=" bg-white text-[#6b6b6b] text-xs py-6 border ">
+        <div className="w-10/12 px-10 flex flex-col lg:flex-row justify-between items-center m-auto">
+          <div>@ ITC Online Limited 2024</div>
+          <ul className="flex gap-5">
+            <li>Terms of use</li>
+            <li>Privacy Policy</li>
+            <li>Sitemap</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
